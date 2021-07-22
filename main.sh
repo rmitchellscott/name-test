f89f5df2a46f87fb39dfa15fd72d1d795e3ae256
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2021-07-22 15:04:05 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,8 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 friendlyName=$(shuf -n 1 "$__dir/adjectives.txt")-$(shuf -n 1 "$__dir/names.txt")
 echo $friendlyName
-echo $GITHUB_SHA
-echo $GITHUB_REF
+echo "SHA: "$GITHUB_SHA
+echo "Ref: "$GITHUB_REF
+echo "Logging into gh..."
 gh auth login --with-token - <<< $GITHUB_TOKEN
 gh pr view --json number,commits,headRefName
