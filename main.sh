56585fac210e8b6da82d9df371667af2256b15c3
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2021-07-22 12:47:18 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
-mapfile -t adjective < $__dir/adjectives
-mapfile -t name < $__dir/names
-friendlyName=${adjective[RANDOM%${#adjective[@]}]-1}-${name[RANDOM%${#name[@]}]-1}
+#mapfile -t adjective < $__dir/adjectives
+#mapfile -t name < $__dir/names
+#friendlyName=${adjective[RANDOM%${#adjective[@]}]-1}-${name[RANDOM%${#name[@]}]-1}
+friendlyName=$(shuf -n 1 "$__dir/adjectives.txt")-$(shuf -n 1 "$__dir/names.txt")
 echo $friendlyName
