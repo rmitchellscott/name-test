41c5539bda28444d3f24ae9550ef9e1918e4f6a8
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2021-07-23 16:54:51 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -11,10 +11,14 @@ _log() {
 _log "SHA: "$GITHUB_SHA
 _log "Ref: "$GITHUB_REF
 _log Verify this is a PR
-#prNum=$(gh pr view --json number --jq .number)
-#if [ ! $? -eq 0 ]; then
-#    _log "We're not operating on a pull request! Aborting."
-#    exit 1
-#fi
+prNum=$(gh pr view --json number --jq .number)
+if [ ! $? -eq 0 ]; then
+    type=$(echo $GITHUB_REF | awk -F/ '{print $1}')
+    if [ $type=="pull" ]; then
+        prNum=$(echo $GITHUB_REF | awk -F/ '{print $3}')
+    else
+        _log "We're not operating on a pull request! Aborting."
+        exit 1
+fi
 prNum=$(echo $GITHUB_REF | awk -F/ '{print $3}')
 environment="pr"$prNum
